e07eb72873f916e56ff037b195b362b3267ac308
nguyenduyy <duyy.uit@gmail.com>
2019-08-31 14:11:09 +0700

    update layouts for adding restaurant

---
+++ app/src/main/java/com/foodbodi/NotifyWaitingForApproval.kt
@@ -0,0 +1,12 @@
+package com.foodbodi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotifyWaitingForApproval : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notify_waiting_for_approval)
+    }
+}
---
+++ app/src/main/java/com/foodbodi/RegisterBusinessInformation.kt
@@ -0,0 +1,12 @@
+package com.foodbodi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterBusinessInformation : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_business_information)
+    }
+}
